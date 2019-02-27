7fb8e48b2cfbb1e91ffbf3b1fb485f6652e90dbe
Digital (Cipher) Lucifer <39482961+dls-cipher@users.noreply.github.com>
2019-02-27 18:53:08 +0700

    testing and development of create account page
    [DELETE] Mistake during initial upload

--- signup-form.php
+++
@@ -1,9 +0,0 @@
-<link href="assets/css/signup-form.css" rel="stylesheet">
-<main class="main-content">
-	<section class="section section-x">
-    <div class="container">
-        <div id=app></div>
-    </div>
-	</section>
-    <script src=assets/js/chunk-vendors.fa2ed579.js> </script> <script src=assets/js/app.98585cf2.js> </script> 
-</main>
