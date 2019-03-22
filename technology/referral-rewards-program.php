ab952e624b1a7ee332a8ff40550c1ef0845aa605
Ross Walker <ross@apasia.tech>
2019-03-22 12:50:32 +0700

    [UPDATE] Content - 'Referral Rewards Program'
    Several updates to the page content including:
    - Grammar
    - Readability
    - Sentence structure
    - SEO (headings and meta-desc)
    - Page formatting

--- technology/referral-rewards-program.php
+++ technology/referral-rewards-program.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="BitShares offers 2 types of membership, Basic Account and Lifetime Membership. Only Lifetime Members benefit from the fee-cashback and 1 level referal program.">
+    <meta name="description" content="BitShares uses an advanced referral programme, built directly into its software, to reward those who refer new users">
     <meta name="keywords" content="bitshares, blockchain, referral, membership">
 	
     <title>Referral Rewards Program | Bitshares Blockchain</title>
@@ -20,7 +20,7 @@
 	
     <!--  Open Graph Tags -->
     <meta property="og:title" content="Referral Rewards Program | BitShares Blockchain">
-    <meta property="og:description" content="BitShares offers 2 types of membership, Basic Account and Lifetime Membership. Only Lifetime Members benefit from the fee-cashback and 1 level referal program.">
+    <meta property="og:description" content="BitShares uses an advanced referral programme, built directly into its software, to reward those who refer new users">
     <meta property="og:image" content="../assets/img/og-img.jpg">
     <meta property="og:url" content="https://bitshares.org/">
     <meta name="twitter:card" content="summary_large_image">
@@ -64,33 +64,33 @@
 		</section>
       <section class="section section-x">
         <div class="container">
-          <p>BitShares has an advanced referral program built directly into its software. Financial networks derive their value primarily from their network effect: more people on the same network increases the value of that network for everyone. BitShares capitalizes on this by rewarding those who sign up new users, and does so in a fully transparent and automated way.</p>
+          <p>BitShares has an advanced referral program built directly into its software. Financial networks derive their value primarily from their network effect: more people on the same network increases the value of that network for everyone. BitShares capitalizes on this in a fully transparent and automated way, by rewarding those who refer new users for membership.</p>
 <br />
-    <h4 id="promoting-infrastructure-development">Promoting Infrastructure Development</h4>
-         <p>The BitShares Referral Program is designed to promote infrastructure development. Decentralized financial networks depend upon their business partners to build an ecosystem of businesses, such as exchanges, merchant services, hosted wallets, lightweight wallets, and block explorers. With the BitShares referral program, merchants can now earn more by advertising their support for the BitShares platform. While other platforms primarily promise lower fees, BitShares offers merchants an ongoing revenue stream.</p>
+    <h2 id="promoting-infrastructure-development">Promoting Infrastructure Development</h2>
+         <p>The BitShares Referral Program is designed to promote infrastructure development. Decentralized financial networks depend upon their business partners to build an ecosystem of businesses, such as: exchanges, merchant services, hosted wallets, lightweight wallets and block explorers. With the BitShares referral program, merchants can now earn more by advertising their support for the BitShares platform. While other platforms primarily promise lower fees, BitShares offers merchants an ongoing revenue stream.</p>
          <p>Free market competition results in many different hosted wallet providers competing to acquire new users. Each of these providers has a financial incentive to advertise their wallets by traditional means, which in turn increases the network effect for the BitShares ecosystem.</p>
 <br />
-    <h4 id="background---paypals-referral-system">Background - PayPal’s Referral System</h4>
-         <p>Bootstrapping a new financial network is very hard, and often becomes a chicken-and-egg problem. Basically, it is difficult to attract new users, because new users will only join if other users have already joined.</p>
-         <p>To overcome this problem, we looked at what companies like <a href="https://paypal.com" target="_blank">PayPal</a> did to reach critical mass.</p>
+    <h2 id="background---paypals-referral-system">Background - PayPal’s Referral System</h2>
+         <p>Bootstrapping a new financial network is very hard, and often becomes a chicken-and-egg problem. It is difficult to attract new users, because new users will only join if other users are already members.</p>
+         <p>To overcome this problem, we looked at how companies like <a href="https://paypal.com" target="_blank">PayPal</a> reached critical mass.</p>
 <blockquote>
          <p><i>PayPal’s big challenge was to get new customers. They tried advertising. It was too expensive. They tried Business Development (BD) deals with big banks. Bureaucratic hilarity ensued. The PayPal team reached an important conclusion: BD didn’t work. They needed organic, viral growth. They needed to give people money.</i></p>
 </blockquote>
 <blockquote>
          <p><i> So that’s what they did. New customers got $10 for signing up, and existing ones got $10 for referrals. Growth went exponential, and PayPal wound up paying $20 for each new customer. It felt like things were working and not working at the same time; 7 to 10% daily growth and 100 million users was good. No revenues and an exponentially growing cost structure were not. Things felt a little unstable. PayPal needed buzz so it could raise more capital and continue on.</i></p>
 </blockquote>
-         <p>Giving people money is both expensive and potentially unsustainable, but it has been proven to work.</p>
+         <p>Giving people money is both expensive and potentially unsustainable, but it works.</p>
 <br />
-    <h4 id="cost-of-customer-acquisition-vs-lifetime-value-of-customer">Cost of Customer Acquisition vs Lifetime Value of Customer</h4>
+    <h2 id="cost-of-customer-acquisition-vs-lifetime-value-of-customer">Cost of Customer Acquisition vs Lifetime Value of Customer</h2>
          <p>BitShares recognizes that a sustainable system must be able to cover all operating expenses from cash flow. In the article <a href="http://www.forentrepreneurs.com/startup-killer/" target="_blank">“Startup Killer: The Cost of Customer Acquisition”</a>, David Skok shared the lessons he learned after watching many failed startups.</p>
          <p><i>However, after closely watching several hundred startups that have failed, I observed that a very large number of these had solved the product/market fit problem, but still failed because they had not found a way to acquire customers at a low enough cost.</i></p>
          <p>A well-balanced business model must have a cost of user acquisition that is much lower than its ability to monetize its customers.</p>
     <center><img class="img-responsive img-thumbnail" src="/images/wellbalanced.svg"/ alt=""></center>
          <p>In the case of BitShares, this means setting transaction fees that are high enough to cover all costs, including the cost of customer acquisition, while still being low enough to compete against real competitors. Most cryptocurrency networks barely charge enough in transaction fees to cover the cost of running the network. They attempt to attract users with low fees. While low fees are important, undervaluing the service provided is counterproductive. For this reason, BitShares charges a price that is much higher than competing cryptocurrencies but much lower than traditional exchanges and payment networks like Dwolla or PayPal.</p>
-         <p>To ensure the cost of customer acquisition is sustainable, the BitShares referral system is designed to always pay out a <i>percentage of <strong>actual profits</strong></i>. BitShares allows the users to draw their own conclusions about the Life Time Value (LTV) of each customer they refer, after educating them about the LTV of customers for similar businesses. We believe that the BitShares network can offer each referrer an average value of $80 per member they sign up, or $40 if they go through an affiliate. This value is realized over time based upon the actual usage of the customers. Even if a customer thinks we are overestimating by a factor of 4, BitShares still offers a larger incentive than PayPal did.</p>
+         <p>To ensure the cost of customer acquisition is sustainable, the BitShares referral system is designed to always pay out a <i>percentage of <strong>actual profits</strong></i>. BitShares allows the users to draw their own conclusions about the Life Time Value (LTV) of each customer they refer, after educating them about the LTV of customers for similar businesses. We believe that the BitShares network can offer each referrer an average value of $80 per member they sign up, or $40 if they go through an affiliate. Realizing this value over time is possible based on the actual usage of the customers. Even if a customer thinks we are overestimating by a factor of 4, BitShares still offers a larger incentive than PayPal did.</p>
 <br />
-    <h4 id="conclusion">Conclusion</h4>
-         <p>BitShares is built for viral growth, and has game-changing, industry-leading incentives.</p>
+    <h2 id="conclusion">Conclusion</h2>
+         <p>BitShares is aim is viral growth, with game-changing, industry-leading incentives.</p>
         </div>
       </section>
     </main>
