5541aefe52822071ae4c555add197ea675860b97
richcg <rich@is1.co>
2019-03-28 22:37:05 -0700

    Create project-listings.php

---
+++ project-listings.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Blockchain and native dApps Download section for BitShares 2.0">
+    <meta name="keywords" content="bitshares, blockchain, wallet, download, access, windows, win, mac, osx, linux, cli, core, ui">
+    <title>Project Listings | BitShares Blockchain</title>
+	<base href="/">
+
+    <!-- Styles -->
+    <link href="assets/css/core.min.css" rel="stylesheet">
+    <link href="assets/css/thesaas.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/css/popup.css" rel="stylesheet" >
+	
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+    <link rel="icon" href="assets/img/favicon.ico">
+	
+    <!--  Open Graph Tags -->
+    <meta property="og:title" content="Project Listings | BitShares Blockchain ">
+    <meta property="og:description" content="Blockchain Project Listings for BitShares 2.0">
+    <meta property="og:image" content="assets/img/og-img.jpg">
+    <meta property="og:url" content="https://bitshares.org/">
+    <meta name="twitter:card" content="summary_large_image">
+	
+</head>
+
+ <body>
+	<div canvas="container">
+	
+    <!-- Topbar Navigation Bar Begins-->
+    <?php require('assets/php/header.php'); ?>
+    <!-- Top Navigation Bar Ends-->
+	
+    <!-- Header -->
+    <header class="header header-inverse inner-headers">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-12 col-lg-8 offset-lg-2">
+
+            <h1>Project Listings</h1>
+            <p class="fs-18 opacity-70">List of dApps built on the BitShares blockchain</p>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- END Header -->
+
+    <!-- Main container -->
+    <main class="main-content">
+      <section class="section section-x">
+        <div class="container">
+			<br />
+			<h4>Project Listings</h4>
+			<ul class="ml-40 ls-none">
+				<li class="mt-20">
+					<a target="_blank" href="https://github.com/bitshares/bitshares-core">Core Team: bitshares-core</a>
+				</li>
+				<li class="mt-20">
+					<a target="_blank" href="https://github.com/bitshares/bitshares-ui">UI Team: bitshares-ui, beet</a>
+				</li>
+				<li class="mt-20">
+					<a target="_blank" href="https://github.com/TrustyFund">Trusty Team: bitshares-community-ui</a>
+				</li>
+				<li class="mt-20">
+					<a target="_blank" href="https://github.com/bitshares/bitshares-mobile-app">BTS++ Team: bitshares-mobile-app</a>
+				</li>
+				<li class="mt-20">
+					<a target="_blank" href="https://github.com/Codaone/DEXBot">DEXBot Team: DEXBot</a>
+				</li>
+			</ul>
+			<br />
+			</div>
+      </section>
+    </main>
+<!-- END Main container -->
+
+<!-- Footer -->
+<?php require('assets/php/footer.php');?>
+<!-- END Footer -->
+
+</div>
+
+<!-- Slide Mobile Menu --->
+<?php require('assets/php/mobilemenu.php');?>
+<!-- End Slide Mobile Menu --->
+
+    <!-- Scripts -->
+    <script src="assets/js/core.min.js"></script>
+    <script src="assets/js/thesaas.js"></script>
+    <script src="assets/js/script.js"></script>
+	<!-- End Scripts -->
+	<!-- Go to Top Button -->
+	  <div id="stop" class="scrollTop bttt-border bttt-round bttt-positionRight bttt-slideFromBottom">
+		<i class='fa fa-chevron-up'></i>
+	  </div>
+	<!-- End Go to Top Button -->
+	  	<script>
+			$(function() {
+				cbpHorizontalMenu.init();
+			});
+		</script>
+	</body>
+</html>
