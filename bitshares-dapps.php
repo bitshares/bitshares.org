0ee8c891f71221a706d04fb1c6bfc179e13dac17
Ross Walker <ross@apasia.tech>
2019-04-11 11:12:54 +0700

    [DRAFT] Update content on bitshares-dapps.php
    First update of draft content as per issue https://github.com/bitshares/bitshares.org/issues/43
    Note: Not ready for publication. Styling/layout and further formatting to be done, including thumbnails for downloads and review of information/links to be included.

--- bitshares-dapps.php
+++ bitshares-dapps.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Blockchain and native dApps Download section for BitShares 2.0">
-    <meta name="keywords" content="bitshares, blockchain, wallet, download, access, windows, win, mac, osx, linux, cli, core, ui">
+    <meta name="keywords" content="bitshares, blockchain, dapps, wallet, download, osx, windows, linux">
     <title>Project Listings | BitShares Blockchain</title>
 	<base href="/">
 
@@ -52,26 +52,84 @@
     <main class="main-content">
       <section class="section section-x">
         <div class="container">
-			<br />
-			<h4>Project Listings</h4>
-			<ul class="ml-40 ls-none">
-				<li class="mt-20">
-					<a target="_blank" href="https://github.com/bitshares/bitshares-core">Core Team: bitshares-core</a>
-				</li>
-				<li class="mt-20">
-					<a target="_blank" href="https://github.com/bitshares/bitshares-ui">UI Team: bitshares-ui, beet</a>
-				</li>
-				<li class="mt-20">
-					<a target="_blank" href="https://github.com/TrustyFund">Trusty Team: bitshares-community-ui</a>
-				</li>
-				<li class="mt-20">
-					<a target="_blank" href="https://github.com/bitshares/bitshares-mobile-app">BTS++ Team: bitshares-mobile-app</a>
-				</li>
-				<li class="mt-20">
-					<a target="_blank" href="https://github.com/Codaone/DEXBot">DEXBot Team: DEXBot</a>
-				</li>
-			</ul>
-			<br />
+	    <br />
+		<h2>BitShares Core software</h2>
+                    <br />
+                    <p>Name: bitshares-core (Active)</p>
+                    <p>Description: BitShares Core is the BitShares blockchain implementation and command-line interface.</p>
+                    <p>Tags: #blockchain #cryptocurrency #bitshares #cli #cpp</p>
+                    <p>More info: <a href='https://dev.bitshares.works/'>Developers portal</a>
+                        &nbsp;
+                        <a target="_blank" href='https://github.com/bitshares/bitshares-core'>GitHub repository</a>
+                    <p>Download(s): <a target="_blank" href='https://github.com/bitshares/bitshares-core/releases/latest'>latest official core release</a>
+                    </p>
+		<br />
+		
+		<h2>BitShares UI (reference wallet)</h2>
+                    <br />
+                    <p>Name: bitshares-ui (Active)</p>
+                    <p>Description: Fully featured Graphical User Interface / Reference Wallet for the BitShares Blockchain, the default dApp for end-users. Available as a client application to install on a PC, or as a web application accessed in a web browser.</p>
+                    <p>Tags: #bitshares #blockchain #cryptocurrency #blockchain-wallet #web-app #react-js</p>
+                    <p>More info: 
+                    &nbsp;    
+                        <a href='https://wallet.bitshares.org' target='_blank'>Web version</a>
+                    &nbsp;
+                    <a target="_blank" href='https://github.com/bitshares/bitshares-ui'>GitHub repository</a>
+                    </p>
+                      <br />
+                    <p>Downloads(s):
+                        <a href='https://github.com/bitshares/bitshares-ui/releases/download/2.0.190227/BitShares.Setup.2.0.190227.exe'><img class="h-25 mt-0 mb-12 mx-0" src="assets/img/windows.svg" alt="Windows"/>&nbsp BitShares.Setup.2.0.190227.exe </a>
+                        <br />   
+                        <a target="_blank" href='https://github.com/bitshares/bitshares-ui/releases/download/2.0.190227/BitShares-2.0.190227.dmg'><img class="h-25 mt-0 mb-12 mx-0" src="assets/img/mac.svg"/ alt="Mac"> &nbsp BitShares-2.0.190227.dmg </a>
+                       <br />
+                        <a target="_blank" href='https://github.com/bitshares/bitshares-ui/releases/download/2.0.190227/BitShares2-light_2.0.190227_amd64.deb'><img  class="h-25 mt-0 mb-12 mx-0" src="assets/img/linux.svg"/ alt="Linux"> &nbsp BitShares2-light_2.0.190227_amd64.deb </a>
+                        <br />
+                        <a target="_blank" href='https://github.com/bitshares/bitshares-ui/releases/'><img  class="h-25 mt-0 mb-12 mx-0" src="assets/img/github.svg"/ alt="GitHub"> &nbsp More releases on GitHub</a>    
+                    </p>   
+		<br />
+            
+            <h2>BitShares Community UI</h2>
+                    <br />
+                    <p>Name: bitshares-community-ui (Part 1 complete, part 2 to follow)</p>
+                    <p>Description: Light and 100% Responsive, BitShares Community DEX/UI - based on Vue.js Framework.</p>
+                    <p>Tags: #bitshares #blockchain #cryptocurrency #blockchain-wallet #web-app #react-js</p>
+                    <p>More info: 
+                        <br />
+                        <a target="_blank" href='https://github.com/bitshares/bitshares-community-ui'><img  class='h-25 mt-0 mb-12 mx-0' src='assets/img/github.svg'/ alt='GitHub'> GitHub repository</a>
+                        <br />
+                        <a target="_blank" href='http://testing.community-ui.bitshares.org/'><img  class='h-25 mt-0 mb-12 mx-0' src='assets/img/tech-bg1.jpg'/ alt='Demo'> Demo (ver 1.2.2)</a>
+                        <br />
+                        <a target="_blank" href='https://bitsharestalk.org/index.php?topic=26873.0'><img  class='h-25 mt-0 mb-12 mx-0' src='assets/img/logo.png'/ alt='Demo'> Talk forum topic</a>
+                    </p>
+                <br />
+			    
+                 <h2>BitShares Mobile App</h2>        
+                 <br />
+                    <p>Name: bitshares-mobile-app (Active)</p>
+                    <p>Description: A mobile app (Android and iOS) for the BitShares blockchain. (Active)</p>
+                    <p>Tags: #bitshares #blockchain #cryptocurrency #blockchain-wallet #web-app #objective-c #kotlin</p>
+                    <p>More info: 
+                        <br />
+                        <a target="_blank" href='https://github.com/bitshares/bitshares-mobile-app'><img  class='h-25 mt-0 mb-12 mx-0' src='assets/img/github.svg'/ alt='GitHub'> GitHub repository</a>
+                        <br />
+                        <a target="_blank" href='https://bitsharestalk.org/index.php?topic=27486.0'><img  class='h-25 mt-0 mb-12 mx-0' src='assets/img/logo.png'/ alt='Demo'> Talk forum topic</a>
+                    </p>    
+                 <br />
+                        
+                <h2>BitShares DEX Liquidity Bot</h2>        
+                 <br />
+                    <p>Name: bitshares-dexbot (Active)</p>
+                    <p>Description: An application for automating liquidity trading and market making in selected markets on the BitShares DEX. (Active)</p>
+                    <p>Tags: #bitshares #blockchain #cryptocurrency #blockchain-wallet #web-app #objective-c #kotlin</p>
+                    <p>More info: 
+                        <br />
+                        <a target="_blank" href='https://github.com/Codaone/DEXBot'><img  class='h-25 mt-0 mb-12 mx-0' src='assets/img/github.svg'/ alt='GitHub'> GitHub repository</a>
+                        <br />
+                        <a target="_blank" href='https://bitsharestalk.org/index.php?topic=27568'><img  class='h-25 mt-0 mb-12 mx-0' src='assets/img/logo.png'/ alt='Demo'> Talk forum topic</a>
+                        <br />
+                        <a target="_blank" href='https://dexbot.info'><img class='h-25 mt-0 mb-12 mx-0' src='assets/img/about-us.jpg'>DEXBot Website</a>
+                    </p>
+                 <br />
 			</div>
       </section>
     </main>
