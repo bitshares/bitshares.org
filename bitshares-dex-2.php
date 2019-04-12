65c8b0133d7c2696f352b96f26f64126fbb82f2d
Ross Walker <ross@apasia.tech>
2019-04-12 17:50:08 +0700

    [CONTENT] BitShares DEX - Native dApps
    Actual content for the menu item About -> Native dApps -> BitShares DEX
    as a introduction and clarification of what is BitShares UI(DEX)/Reference wallet
    
    Warning: WORKING DRAFT
    Review needed.
    
    To be added:
    
    - Clarification that BitShares DEX is just a software showcase development/utilization of all the functionalities of entire ecosystem
    - Clarification that BitShares UI as dapp is not legally "Exchange" or has ever developed gateway for any currency (deposits/withdrawals), and that gateway owners are legit entities built using BitShares technology.
    - Screenshots and showcase of a software
    - Better wording
    
    Merge can be done after commenting, editing PR. 
    
    Thank you

---
+++ bitshares-dex-2.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="The BitShares DEX is a dApp built upon the blockchain for trading of cryptographic tokens and assets.">
+    <meta name="keywords" content="decentralized exchange, blockchain, trading, cryptocurrency, BitShares">
+    <title>Decentralized Exchange (DEX) | BitShares dApps</title>
+    <base href="/">
+
+    <!-- Styles -->
+    <link href="assets/css/core.min.css" rel="stylesheet">
+    <link href="assets/css/thesaas.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/css/popup.css" rel="stylesheet" >
+	
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+    <link rel="icon" href="assets/img/favicon.ico">
+	
+    <!--  Open Graph Tags -->
+    <meta property="og:title" content="The Decentralized EXchange (DEX) | BitShares dApps">
+    <meta property="og:description" content="The BitShares DEX is a dApp built upon the blockchain for exchange of cryptographic tokens and assets.">
+    <meta property="og:image" content="assets/img/og-img.jpg">
+    <meta property="og:url" content="https://bitshares.org/">
+    <meta name="twitter:card" content="summary_large_image">
+	
+</head>
+
+ <body>
+	<div canvas="container">
+	
+    <!-- Topbar Navigation Bar Begins-->
+    <?php require('assets/php/header.php'); ?>
+    <!-- Top Navigation Bar Ends-->
+	
+    <!-- Header -->
+    <header class="header header-inverse inner-headers">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-12 col-lg-8 offset-lg-2">
+
+            <h1>Decentralized Exchange (DEX) | BitShares dApps</h1>
+            <p class="fs-18 opacity-70">Decentralized Exchange built on the BitShares blockchain</p>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- END Header -->
+    <!-- Main container -->
+    <main class="main-content">
+		</section>
+      <section class="section section-x">
+        <div class="container">
+          
+        
+            <h2>Decentralized means direct</h2>
+            <p>The decentralized exchange (further denoted simply as the DEX) allows for direct exchange of digital goods traded in the BitShares ecosystem. A decentralized exchange has a very particular set of advantages over traditional centralized exchanges, some highlights of which are below. Although the BitShares DEX comes with all of these features and many more, it is up to the token holders and users to leverage them in full or only partially.</p>
+
+    <br />
+            <h2>Separation of Powers:</h2>
+            <h3>Refers to the division of responsibilities into distinct branches.</h3>
+            <p><em>There is no reason why the same entity needs to be responsible for issuing IOUs and for processing the order book. In BitShares, order matching is performed by the protocol, which is unaware of implications concerning the involved assets.</em></p><br />
+    <br />
+            <h2>Global Unified Order Book:</h2>
+            <h3>The BitShares Blockchain provides the logic and backend to keep one global ledger for all.</h3>
+            <p><em>Since BitShares is global, an Internet connection is all that is required for anyone to use the DEX for trading, bringing the world’s liquidity to a single decentralized order book.</em></p>
+    <br />
+            <h2>Transactions:</h2>
+            <h3>Any user interacting with the DEX is doing so via the Blockchain, authentic and authorized.</h3>
+            <p><em>Transactions are constructed then transmitted to the network, containing instructions the user wants to perform. The most simple form is the transfer operation, which contains sender, receiver, amount, optionally with an encrypted memo.</em></p><br />
+    <br />
+            <h2>Trade Almost Anything:</h2>
+            <h3>The BitShares DEX is asset agnostic.</h3>
+            <p><em>On the BitShares DEX users can trade any pair, although some pairs may end up with low liquidity, such as SILVER:GOLD, other pairs such as USD:EUR for FOREX trading will see larger volume.</em></p>
+    <br />
+            <h2>Decentralized:</h2>
+            <h3>Run across the world simultaneously on many nodes.</h3>
+            <p><em>The DEX is decentralized across the globe. This not only means that there is no single point of failure, but it also implies that the BitShares exchange is open for trading 24/7 because it’s always daytime somewhere.</em></p>
+    <br />  
+            <h2>Scalable:</h2><br />
+            <h3>Capable of 100,000 tp/s (transactions per second) with the network to go with it.</h3>
+            <p><em>Delegated Proof of Stake ensures the BitShares network can confirm transactions in an average of just 1 second, limited only by the speed of light. See <a href='/technology/industrial-performance-scalability'>Industrial Performance and Scalability</a> for more</em></p>
+            
+    <br />
+            <h2>Secure:</h2>
+            <h3>Human-readable account names that must be registered together with public-keys in the blockchain.</h3>
+            <p><em>Your funds and trades are secured with industry-grade elliptic curve cryptography. No one will be able to access your funds unless you let them. To further strengthen the security, users are able to setup escrow and multi-signature schemes.</em></p>
+    <br />
+
+           <h2>Try it out</h2>
+            
+          <br />
+        </div>
+      </section>
+    </main>
+<!-- END Main container -->
+
+
+<!-- Footer -->
+<?php require('assets/php/footer.php');?>
+<!-- END Footer -->
+
+</div>
+
+<!-- Slide Mobile Menu --->
+<?php require('assets/php/mobilemenu.php');?>
+<!-- End Slide Mobile Menu --->
+
+    <!-- Scripts -->
+    <script src="assets/js/core.min.js"></script>
+    <script src="assets/js/thesaas.js"></script>
+    <script src="assets/js/script.js"></script>
+	<!-- End Scripts -->
+	<!-- Go to Top Button -->
+	  <div id="stop" class="scrollTop bttt-border bttt-round bttt-positionRight bttt-slideFromBottom">
+		<i class='fa fa-chevron-up'></i>
+	  </div>
+	<!-- End Go to Top Button -->
+	  	<script>
+			$(function() {
+				cbpHorizontalMenu.init();
+			});
+		</script>
+	</body>
+</html>
