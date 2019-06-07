25ffbbd93fefd52e629e058ec682640a82983838
Stefan Schießl <stefan.schiessl@blockchainprojectsbv.com>
2019-06-07 08:00:41 +0200

    Update stakeholder-approved-funding.php

--- technology/stakeholder-approved-funding.php
+++ technology/stakeholder-approved-funding.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="BitShares blockchain is self-funding and has a reserve pool self-sustained by fees, whereby Stakeholder-Approved Funding direct where blockchain budgets go.">
+    <meta name="description" content="BitShares blockchain is self-funding and has a reserve pool self-sustained by fees, whereby BTS Holder Approved Funding direct where blockchain budgets go.">
     <meta name="keywords" content="bitshares, blockchain, stakeholder approved funding">
 
     <title>BTS Holder Approved Funding | BitShares Blockchain</title>
