049ceba297e2ae891781990f6af3774f65bd8b8c
richcg <rich@is1.co>
2019-06-13 10:08:29 -0700

    Update mobilemenu.php

--- assets/php/mobilemenu.php
+++ assets/php/mobilemenu.php
@@ -5,64 +5,47 @@
 		<nav class="overlay-menu h-full mt-25">
 			<div class="containerz">
 				<div class="main">
-					<nav id="cbp-hrmenu" class="cbp-hrmenu">
+										<nav id="cbp-hrmenu" class="cbp-hrmenu">
 						<ul>
 							<li><a href="/">HOME</a></li>
 							<li>
-								<a class="dropdown" href="#">ABOUT</a>
+								<a class="dropdown" href="#">Docs</a>
 								<div class="cbp-hrsub">
 									<div class="cbp-hrsub-inner">
-										<div class="menu-part">
-										<a class="fw-600" href="#">DEVELOPMENT</a>
+										<div class="menu-comm">
 											<ul>
-												<li class="ml-20"><a target="_blank" href="https://dev.bitshares.works">Dev Integration Guide</a></li>
-												<li class="ml-20"><a target="_blank" href="https://bitshares.org/doxygen/">Doxygen</a></li>
-												<li class="ml-20"><a target="_blank" href="https://github.com/bitshares">GitHub</a></li>
+												<li class="ml-20"><a target="_blank" href="https://docs.bitshares.org">BitShares Docs</a></li>
+												<li class="ml-20"><a target="_blank" href="https://dev.bitshares.works">Integration for Developers</a></li>
+												<li class="ml-20"><a target="_blank" href="https://doxygen.bitshares.org">Doxygen</a></li>
+												<li class="ml-20"><a target="_blank" href="https://github.com/bitshares-foundation/bitshares.foundation/blob/master/download/articles/BitSharesBlockchain.pdf">Revised Whitepaper</a></li>
 											</ul>
 										</div>
-									</div>
-									
-									<div class="cbp-hrsub-inner mt-20">
-										<div class="menu-part">
-										<a class="fw-600" href="#">NATIVE dAPPS</a>
-											<ul>
-												<li class="ml-20"><a target="_blank" href="https://bitshares.org/wallet">BitShares DEX</a></li>
-											</ul>
-										</div>
-									</div>
-									
-									<div class="cbp-hrsub-inner mt-20">
-										<div class="menu-part">
-											<ul>
-												<li class="ml-20"></li><a class="nav-link" target="_blank" href="https://how.bitshares.works">Documentation</a></li>
-											</ul>
-										</div>
-									</div>
+									</div><!-- /cbp-hrsub-inner -->
 								</div><!-- /cbp-hrsub -->
 							</li>
-												
+							<li><a target="_blank" href="https://news.bitshares.org">NEWS</a></li>				
 							<li>
 								<a class="dropdown" href="#">TECHNOLOGY</a>
 								<div class="cbp-hrsub">
 									<div class="cbp-hrsub-inner">
 										<div class="menu-comm">
 											<ul>
-												<li class="ml-20"></li><a href="technology/decentralized-asset-exchange">Decentralized&nbsp;Asset&nbsp;Exchange</a></li>
-												<li class="ml-20"></li><a href="technology/delegated-proof-of-stake-consensus">DPoS&nbsp;Consensus</a></li>
-												<li class="ml-20"></li><a href="technology/dynamic-account-permissions">Dynamic&nbsp;Account&nbsp;Permissions</a></li>
-												<li class="ml-20"></li><a href="technology/named-accounts">Named&nbsp;Accounts</a></li>
-												<li class="ml-20"></li><a href="technology/industrial-performance-scalability">Performance&nbsp;and&nbsp;Scalability</a></li>
-												<li class="ml-20"></li><a href="technology/price-stable-cryptocurrencies">Price&#8209;Stable&nbsp;Cryptocurrencies</a></li>
-												<li class="ml-20"></li><a href="technology/recurring-scheduled-payments">Recurring&nbsp;&&nbsp;Scheduled&nbsp;Payments</a></li>
-												<li class="ml-20"></li><a href="technology/referral-rewards-program">Referral&nbsp;Rewards&nbsp;Program</a></li>
-												<li class="ml-20"></li><a href="technology/stakeholder-approved-funding">Stakeholder&nbsp;Approved&nbsp;Funding</a></li>
-												<li class="ml-20"></li><a href="technology/user-issued-assets">User-Issued&nbsp;Assets</a></li>
+												<li class="ml-20"><a href="technology/decentralized-asset-exchange">Decentralized&nbsp;Asset&nbsp;Exchange</a></li>
+												<li class="ml-20"><a href="technology/delegated-proof-of-stake-consensus">DPoS&nbsp;Consensus</a></li>
+												<li class="ml-20"><a href="technology/dynamic-account-permissions">Dynamic&nbsp;Account&nbsp;Permissions</a></li>
+												<li class="ml-20"><a href="technology/named-accounts">Named&nbsp;Accounts</a></li>
+												<li class="ml-20"><a href="technology/industrial-performance-scalability">Performance&nbsp;and&nbsp;Scalability</a></li>
+												<li class="ml-20"><a href="technology/price-stable-cryptocurrencies">Price&#8209;Stable&nbsp;Cryptocurrencies</a></li>
+												<li class="ml-20"><a href="technology/recurring-scheduled-payments">Recurring&nbsp;&&nbsp;Scheduled&nbsp;Payments</a></li>
+												<li class="ml-20"><a href="technology/referral-rewards-program">Referral&nbsp;Rewards&nbsp;Program</a></li>
+												<li class="ml-20"><a href="technology/stakeholder-approved-funding">Stakeholder&nbsp;Approved&nbsp;Funding</a></li>
+												<li class="ml-20"><a href="technology/user-issued-assets">User-Issued&nbsp;Assets</a></li>
 											</ul>
 										</div>
 									</div><!-- /cbp-hrsub-inner -->
 								</div><!-- /cbp-hrsub -->
 							</li>
-							<li><a target="_blank" href="https://www.bitshares.foundation/papers/BitSharesBlockchain.pdf">WHITEPAPER</a></li>
+							<li><a target="_blank" href="https://wallet.bitshares.org">BitShares UI</a></li>
 							<li><a href="download">DOWNLOAD</a></li>
 							<li><a href="contact">CONTACT</a></li>
 						</ul>
